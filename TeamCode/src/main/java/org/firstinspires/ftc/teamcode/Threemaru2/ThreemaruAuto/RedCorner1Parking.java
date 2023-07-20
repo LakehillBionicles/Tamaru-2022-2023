ec934c2a3b5f657c8d6910c6b16c428303aa19be
Austin <arutherford@lakehillprep.org>
2023-07-20 14:46:08 -0500

    Austin 6/12/2023

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Threemaru2/ThreemaruAuto/RedCorner1Parking.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Threemaru2/ThreemaruAuto/RedCorner1Parking.java
@@ -9,43 +9,51 @@ import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.teamcode.NewRoadRunnerTest.drive.SampleMecanumDrive;
 import org.firstinspires.ftc.teamcode.NewRoadRunnerTest.trajectorysequence.TrajectorySequence;
 import org.firstinspires.ftc.teamcode.Threemaru.Auto4.ThreemaruAutoBase;
-
 @Config
 @Autonomous (group = "Auto Testing")
-public class RedCorner1 extends Threemaru2AutoBase {
+public class RedCorner1Parking extends Threemaru2AutoBase{
     @Override
     public void runOpMode() {
         robot.init(hardwareMap);
         super.runOpMode();
         initAuto();
         SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
-
-        TrajectorySequence traj1 = drive.trajectorySequenceBuilder(new Pose2d())
+        TrajectorySequence one = drive.trajectorySequenceBuilder(new Pose2d())
+                .splineToLinearHeading(new Pose2d(6, 0), Math.toRadians(0))//6,0,0
+                .turn(Math.toRadians(91))
+                .waitSeconds(2)
+                .forward(23)
+                .turn(Math.toRadians(-98))
+                .waitSeconds(2)
+                .splineToLinearHeading(new Pose2d(30, 21), Math.toRadians(0))
+                .build();
+        TrajectorySequence two = drive.trajectorySequenceBuilder(new Pose2d())
+                .forward(34)
+                .build();
+        TrajectorySequence three = drive.trajectorySequenceBuilder(new Pose2d())
                 .splineToConstantHeading(new Vector2d(6, 0), Math.toRadians(0))//6,0,0
                 .turn(Math.toRadians(-89))//-89
                 .forward(23)//23
                 .turn(Math.toRadians(101))//101
                 .waitSeconds(2)//2
                 .splineToLinearHeading(new Pose2d(46, -21), Math.toRadians(-3))
-                .addSpatialMarker(new Vector2d(46, -21), () -> armToPosition(3000))
-                .waitSeconds(1)
                 .build();
-
-        TrajectorySequence traj2 = drive.trajectorySequenceBuilder(traj1.end())
-                .forward(6)
-                .build();
-
         waitForStart();
 
         if (isStopRequested()) return;
         armToPosition(100);
-        drive.followTrajectorySequence(traj1);
-        distDriveStar(-1, 10);
-        turretTimeBasedReset();
-        double starDist = robot.distSensorStar.getDistance(DistanceUnit.CM);
-        extensionToDistStar(starDist);
-        openHand();
-        turretToPosition(1800);
+        if(sideOfSleeve == 1) {
+            drive.followTrajectorySequence(one);
+        }
+        if(sideOfSleeve == 2) {
+            drive.followTrajectorySequence(two);
+        }
+        if(sideOfSleeve == 11) {
+            drive.followTrajectorySequence(three);
+        }
+        else{
+            drive.followTrajectorySequence(one);
+        }
         //drive.followTrajectorySequence(traj2);
     }
 }
