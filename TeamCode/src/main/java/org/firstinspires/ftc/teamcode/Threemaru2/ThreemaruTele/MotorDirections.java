94a636b6b028e165e88a18b4e284a516bc6be129
Austin <arutherford@lakehillprep.org>
2023-08-14 13:04:31 -0500

    Austin 6/12/2023

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Threemaru2/ThreemaruTele/MotorDirections.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Threemaru2/ThreemaruTele/MotorDirections.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.Threemaru2.ThreemaruTele;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 import org.firstinspires.ftc.teamcode.Threemaru2.Threemaru2Hardware;
 
@@ -10,9 +11,11 @@ public class MotorDirections extends LinearOpMode {
     Threemaru2Hardware robot = new Threemaru2Hardware();
         public void runOpMode() {
             robot.init(hardwareMap);
-
+            robot.bsd.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            robot.bpd.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            robot.fsd.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            robot.fpd.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
             waitForStart();
-
             while (opModeIsActive()) {
                     robot.fpd.setPower(.5);
                     robot.bpd.setPower(.5);
